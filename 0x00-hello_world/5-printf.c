3c036592b73ee6e49c5ab6630b97a5b5727caf0c
Vivian <vdagbue@gmail.com>
2023-03-16 11:43:55 -0700

    Add: C-Hello World Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function starts here
+ *
+ * Description: printf function send a string to the standard output
+ *
+ * Return: Return(0) stops the compiler
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
