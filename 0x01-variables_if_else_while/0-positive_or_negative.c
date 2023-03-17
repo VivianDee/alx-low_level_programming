5b5e0e3b9e85719ed7a01c2507e97e017eefdf71
Vivian <vdagbue@gmail.com>
2023-03-17 06:17:05 -0700

    Add: C-Variables, if, else, while README and task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @n is given a random value
+ *
+ * Description: An IF statement checks if the value of n is positive, negative
+ * or zero and prints out n and a string. 
+ *
+ * Return: Always 0 (Success) 
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf ("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf ("%d is zero\n", n);
+	}else 
+	{
+		printf ("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,14 @@
+ C - Variables, if, else, while
+
+This repository contains tasks I took on during the Alx program. The script are based on the following C concepts:
+
+Keywords and identifiers
+integers
+Arithmetic Operators in C
+If statements in C
+ifelse statement
+Relational operators
+Logical operators
+while loop in C
+While loop and
+ascii 
