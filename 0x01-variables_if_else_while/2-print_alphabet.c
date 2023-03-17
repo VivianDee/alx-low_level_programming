637e3a9d3ba5df4addbe711cc47094d5f5cd15f1
Vivian <vdagbue@gmail.com>
2023-03-17 09:59:47 -0700

    Add: C - Variables, if, else, while Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: putchar prints out alphabets a-z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	c = 97;
+	while(c != 123)
+	{
+		putchar(c);
+		c = c+1;
+	}
+	putchar('\n');
+	return (0);
+}
