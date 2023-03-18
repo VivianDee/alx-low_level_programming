c6fbbd7945cc9e4e4a7298043aed7876fc478404
Vivian <vdagbue@gmail.com>
2023-03-17 23:05:10 -0700

    Fix: C - Hello, World Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
 
 /**
@@ -9,6 +12,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
