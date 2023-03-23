7fad99cb7fc5f2c897213cd8ccc26ec394de7949
Vivian <vdagbue@gmail.com>
2023-03-23 01:14:14 -0700

    Add: README file, _putcharfunction file and main.h header file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase characters.
+ * @c: Lowercase or uppercase alphabeth.
+ *
+ * Return: 1 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
