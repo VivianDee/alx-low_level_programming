3a2429f8c9ad71a41cdd5b73eab501c51a13e1b7
Vivian <vdagbue@gmail.com>
2023-03-24 08:22:00 -0700

    Fix: Output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,32 @@
 #include "main.h"
 
 /**
+ * print_number - A function that prints a number
+ * @n: A number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	} else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+}
+
+/**
  * print_to_98 - A function that prints all natural numbers from n to 98
+ * @n: A number
  *
  * Return: Always 0 (Success)
  */
@@ -9,42 +34,25 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n <= 98) {
-		for (i = n; i <= 98; i++) {
-			if(i < 0)
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_number(i);
+			if (i != 98)
 			{
-				_putchar('-');
-				i = i * -1;
-				_putchar(i + '0');
-			}
-			if(i < 10 && i >= 0)
-			{
-				_putchar(i + '0');
-			}
-			if (i > 10)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			if (i != 98) {
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
-	} else {
-		for (i = n; i >= 98; i--) {
-			if(i >= 100)
+	} else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_number(i);
+			if (i > 98)
 			{
-				_putchar(i / 100 + '0');
-				_putchar((i / 10) % 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			if (i < 100)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			if (i != 98) {
 				_putchar(',');
 				_putchar(' ');
 			}
