8b1d3f98864beb32287cb4710e6e23c6906d2f78
Vivian <vdagbue@gmail.com>
2023-04-10 05:13:03 -0700

    Fix: While loop

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - A function that compares two strings and returns 1
+ * wildcmp_recursive - A function that compares two strings and returns 1
  *if the strings can be considered identical, otherwise return 0.
  * @s1: A string to be compared with string s2
  * @s2: A string to be compared with string s1
@@ -10,28 +10,37 @@
  * - 1 if the strings are identical
  * - 0 if the string are not identical
  */
-
-int wildcmp(char *s1, char *s2)
+int wildcmp_recursive(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
-	}
-	if (*s2 == '*')
+	} else if (*s2 == '*')
 	{
-		while (*s1 != '\0')
+		if (wildcmp_recursive(s1, s2 + 1))
 		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
+			return (1);
+		} else if (*s1 != '\0' && wildcmp_recursive(s1 + 1, s2))
+		{
+			return (1);
 		}
-		return (wildcmp(s1, s2 + 1));
-	}
-	if (*s1 == *s2)
+			return (0);
+	} else if (*s1 == *s2 || (*s2 == '?' && *s1 != '\0'))
 	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp_recursive(s1 + 1, s2 + 1));
 	}
-	return (0);
+		return (0);
+}
+
+/**
+ * wildcmp - A function that compares two strings and returns 1
+ * @s1: A string to be compared with string s2
+ * @s2: A string to be compared with string s1
+ *
+ * Return: wildcmp_recursive
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcmp_recursive(s1, s2));
 }
