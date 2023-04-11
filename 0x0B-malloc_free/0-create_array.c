f1b4964b22a574676e11877caf2af4e18b74dca1
Vivian <vdagbue@gmail.com>
2023-04-11 08:17:58 -0700

    Fix: allocted space

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *p = (char *)malloc(size * sizeof(char) + 1);
+	char *p = (char *)malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
