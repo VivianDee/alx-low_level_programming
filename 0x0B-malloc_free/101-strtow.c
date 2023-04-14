6c42fe04f17f4479b20f9f382307930a4b59b981
Vivian <vdagbue@gmail.com>
2023-04-14 08:08:31 -0700

    Add: Advnced task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * strtow - A function that splits a string into words.
@@ -7,10 +8,46 @@
  *
  * Return: str (Success) NULL (failure)
  */
-
 char **strtow(char *str)
 {
-	char *s = str;
+	unsigned long int i = 0, len = 0, word = 0, j = 0;
+	char **p;
 
-	return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+			len++;
+		if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
+			word++;
+	}
+	p = (char **)malloc((word + 1) * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	p[word] = NULL;
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (str[i] != ' ')
+		{
+			if (i == 0 || str[i - 1] == ' ')
+			{
+				p[j] = (char *)malloc((len + 1) * sizeof(char));
+				if (p[j] == NULL)
+				{
+					while (j--)
+						free(p[j]);
+					free(p);
+					return (NULL);
+				}
+				len = 0;
+				p[j][len] = str[i];
+				j++;
+			} else
+			{
+				p[j - 1][len] = str[i];
+			}
+			len++;
+		}
+	}
+	p[j] = NULL;
+	return (p);
 }
