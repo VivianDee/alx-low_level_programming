9996f3307564a50759ab9ba025a64e28bea42891
Vivian <vdagbue@gmail.com>
2023-04-13 20:14:04 -0700

    Advanced task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * main - Entry point
+  * @argc: Argument count
+  * @argv: Argument vector
+  *
+  * Return: 0 (success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int multiply;
+
+	if (argc == 3)
+	{
+		multiply = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multiply);
+	} else
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	return (0);
+}
