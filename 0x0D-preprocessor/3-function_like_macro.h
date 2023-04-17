6a66d17f615dbdf77b8d0d97cb34fdbc8e36558f
Vivian <vdagbue@gmail.com>
2023-04-17 04:19:41 -0700

    Fix: Header guard

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef ABS
+#ifndef ABS_X
+#define ABS_X
 
 #define ABS(x) (((x) < (0)) ? -(x) : (x))
 
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,5 @@
-#ifndef SUM
+#ifndef SUM_X_Y
+#define SUM_X_Y
 
 #define SUM(x, y) ((x) + (y))
 
