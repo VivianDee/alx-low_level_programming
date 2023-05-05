a1299943bdb52d34ef777adc0404512c3b0ef65a
Vivian <vdagbue@gmail.com>
2023-05-05 01:19:05 -0700

    Fix: Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * binary_to_decimal - A function that converts a binary number to a decimal
- * @num: binary number
- * @len: length of the binary number
- *
- * Return: binary (success)
- */
-unsigned int binary_to_decimal(unsigned int num, unsigned int len)
-{
-	unsigned int i = 0, binary = 0, j = 0;
-
-	for (i = 0; i < len; i++)
-	{
-		j = num % 10;
-		binary += j << i;
-		num = num / 10;
-	}
-
-	return (binary);
-}
-
-/**
   * binary_to_uint - A function that converts a binary number to a decimal
   * @b: String of a binary number
   *
@@ -30,17 +9,23 @@ unsigned int binary_to_decimal(unsigned int num, unsigned int len)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int num = 0, len = 0;
+	unsigned int decimal = 0, len = 0, j = 0;
 
 	if (b == NULL)
 		return (0);
 
-	num = atoi(b);
 	len = strlen(b);
-	if (len > 1 && num > 1)
-		return (binary_to_decimal(num, len));
-	else if (len == 1 && num == 1)
-		return (1);
-	else
-		return (0);
+	for (int i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			decimal += (b[i] - '0') << j;
+		}
+		else
+			return (0);
+
+		j++;
+	}
+
+	return (decimal);
 }
