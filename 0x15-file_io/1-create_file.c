dd42da9fbeba522389d890dffec4fd8c60d95fe7
Vivian <vdagbue@gmail.com>
2023-05-08 07:43:44 -0700

    Fix: open function flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_RDWR | O_CREAT, 0600);
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (!fp)
 		return (-1);
 
+	if (text_content == NULL)
+		return(-1);
 	bytes_written = write(fp, text_content, strlen(text_content));
 	if (bytes_written < 0)
 	{
