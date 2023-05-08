c86935ba7ff7855582f2c1ea5cd4c7be1fe2a431
Vivian <vdagbue@gmail.com>
2023-05-08 09:55:34 -0700

    Advanced task

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+
+#define BUFFER_SIZE 1024
+
+
+/**
+  * print_error - Print an error message to stderr and exit with status code 98.
+  *
+  * @message: The error message to print.
+  */
+void print_error(const char *message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(98);
+}
+
+/**
+  * print_elf_header - Print the information contained in an ELF header.
+  *
+  * @header: A pointer to the ELF header to print.
+  */
+void print_elf_header(const Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++) {
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");
+	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2MSB ? "big endian" : "little endian");
+	printf("  Version:                           %d%s\n", header->e_ident[EI_VERSION], header->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %d\n", header->e_type);
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+	printf("  Start of program headers:          %ld (bytes into file)\n", header->e_phoff);
+	printf("  Start of section headers:          %ld (bytes into file)\n", header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
+}
+
+
+/**
+  * main - Entry point
+  *
+  * @argc: The number of command-line arguments.
+  * @argv: An array of strings containing the command-line arguments.
+  *
+  * Return: 0 on success, or a non-zero value on failure.
+  */
+int main(int argc, char *argv[])
+{
+	ssize_t bytes_read;
+	Elf64_Ehdr header;
+	const char *filename = argv[1];
+	int fd = open(filename, O_RDONLY);
+
+	if (argc != 2)
+	{
+		print_error("Usage: elf_header elf_filename");
+	}
+	if (fd == -1)
+	{
+		print_error("Could not open file");
+	}
+
+	bytes_read = read(fd, &header, sizeof(header));
+	if (bytes_read == -1 || bytes_read != sizeof(header))
+	{
+		print_error("Could not read ELF header");
+	}
+
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		print_error("File is not an ELF file");
+	}
+
+	print_elf_header(&header);
+
+	close(fd);
+	return 0;
+}
