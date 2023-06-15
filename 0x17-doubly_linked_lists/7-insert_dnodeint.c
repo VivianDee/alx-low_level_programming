f529839e37aa3c9372a4c2242a6abbb0cf0bd06c
Vivian Dagbue <vdagbue@gmail.com>
2023-06-15 08:23:34 -0700

    Fix: Case idx is zero

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,32 +11,40 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	dlistint_t *current = *h;
-	dlistint_t *next = NULL;
+	dlistint_t *node = NULL, *current = *h;
 	unsigned int count = 0;
 
+	if (*h == NULL)
+		return (NULL);
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
 	node->n = n;
 	node->next = NULL;
 	node->prev = NULL;
-
-	if (*h == NULL && idx == 0)
+	if (idx == 0)
 	{
-		*h = node;
-	}
-	else
-	{
-		while (current != NULL && count != (idx - 1))
+		node->next = *h;
+		if (*h != NULL)
 		{
-			current = current->next;
-			count++;
+			(*h)->prev = node;
 		}
-		next = current->next;
-		next->prev = node;
-		node->next = next;
-		node->prev = current;
-		current->next = node;
+		*h = node;
+		return (node);
 	}
-
+	while (current != NULL && count != (idx - 1))
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+	{
+		free(node);
+		return (NULL);
+	} else if (current->next != NULL)
+		current->next->prev = node;
+	node->next = current->next;
+	node->prev = current;
+	current->next = node;
 	return (node);
 }
