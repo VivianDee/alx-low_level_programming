c64ff99f92877c2aeec80ebbafe8c62b3aabfd83
Vivian Dagbue <vdagbue@gmail.com>
2023-07-07 09:12:14 -0700

    Fix: Memory leaks in extreme cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,39 +11,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item = NULL;
+	hash_node_t *item = NULL, *temp = NULL;
 	unsigned long int index = 0;
 
+	if (!key || !ht || key[0] == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
 	item = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (!item)
 		return (0);
-	if (!key)
-		return (0);
-	index = key_index((const unsigned char *)key, 1024);
-	ht->size = 1024;
 	item->key = (char *)malloc(strlen(key) + 1);
 	if (!item->key)
-		return (0);
-
+	{
+		free(item);
+		return (0); }
 	item->value = (char *)malloc(strlen(value) + 1);
 	if (!item->value)
-		return (0);
+	{
+		free(item->key);
+		free(item);
+		return (0); }
 	strcpy(item->key, key);
 	strcpy(item->value, value);
-
 	if (!ht->array[index])
 	{
+		item->next = ht->array[index];
 		ht->array[index] = item;
-		return (1);
 	}
-	else
-	{
-		free(ht->array[index]->key);
-		free(ht->array[index]->value);
-		free(ht->array[index]);
-		ht->array[index] = NULL;
-		ht->array[index] = item;
-		return (1);
-	}
-	return (0);
+	return (1);
 }
