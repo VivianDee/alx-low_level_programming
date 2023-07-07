f1891a9336ac93517cc2a326d3e13635e101e90f
Vivian Dagbue <vdagbue@gmail.com>
2023-07-07 09:22:07 -0700

    Fix: Extreme cases

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,14 +12,18 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index = NULL;
 	hash_node_t *item = NULL;
 
+	if (!ht || !key || *key == "\0")
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[index];
-	if (item)
+	while (item)
 	{
 		if (strcmp(item->key, key) == 0)
 			return (item->value);
+		item = item->next;
 	}
 
 	return (NULL);
