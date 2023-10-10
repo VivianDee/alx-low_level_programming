b5e5d66b4e9c5aca5dce17263aefde7ec12de94d
VivianDee <vdagbue@gmail.com>
2023-10-10 09:04:13 -0700

    Fix: Header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,6 @@
 #ifndef LIST
 #define LIST
+#include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t size);
