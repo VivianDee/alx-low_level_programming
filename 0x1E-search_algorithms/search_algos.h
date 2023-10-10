ac6f79d7fb13cf8b405db036c6ff089952ac63c0
VivianDee <vdagbue@gmail.com>
2023-10-10 08:58:10 -0700

    Add: Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef LIST_H
+#define LIST_H
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
